88d422442977a58c01be9e1952d6b2457c52909a
JorPoon <jormpoon@gmail.com>
2020-08-27 16:29:26 -0400

    created basic navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import NavBar from './components/nav/NavBar'
 
 function App() {
   return (
     <div className="App">
-      <Typography color="primary" variant="h1" component="h2">
-        h1. Heading
-      </Typography>
+      <NavBar/>
     </div>
   );
 }
