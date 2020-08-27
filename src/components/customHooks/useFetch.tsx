dcd85c8eb2232d2b2a13573cff1a156aa378babd
JorPoon <jormpoon@gmail.com>
2020-08-27 17:52:15 -0400

    created custom fetch hook

--- src/components/customHooks/useFetch.tsx
+++ src/components/customHooks/useFetch.tsx
@@ -1,11 +1,28 @@
 import React, { useState, useEffect } from 'react'
 
 
-export default function useFetch() {
-    const [data, setData] = useState<Array<object>>([])
+export default function useFetch(url: string) {
+
+    const [response, setResponse] = useState<any>(null)
+    const[error, setError] = useState<any>(null)
+    const [isLoading, setIsLoading] =useState<boolean>(false)
 
     useEffect(() => {
-        
-    })
+        const fetchData = async () => {
+            setIsLoading(true)
 
+            try {
+                const res = await fetch(url)
+                const json = await res.json()
+
+                setResponse(json);
+                setIsLoading(false)
+                
+            } catch (error) {
+                setError(error)
+            }   
+        };
+        fetchData();
+    },[url])
+    return {response, error, isLoading}
 }
