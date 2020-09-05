25c5b3a48e1a43da2980cd79d92e309b6b9b21cc
JorPoon <jormpoon@gmail.com>
2020-09-05 13:54:16 -0400

    setting up nomination list cards and functions

--- src/components/movies/NominateCard.tsx
+++ src/components/movies/NominateCard.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import Card from '@material-ui/core/Card'
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -7,40 +8,15 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-import { makeStyles } from '@material-ui/core/styles';
+const NominateCard = () => {
 
-const useCardStyle = makeStyles({
-    root: {
-    //   minWidth: '250px',
-      width: '300px',
-      margin: '10px 0'
-    },
-    media: {
-        paddingTop: '56.25%',
-        height: '450px'
-    }
-  });
-  
-
-
-interface Movie {
-    title: string,
-    year: string,
-    key: string,
-    poster: string,
-    nominate: any
-}
-
-const MovieCard = (props:Movie) => {
-    const classes = useCardStyle()
-
-    const nominateMovie = (event: React.MouseEvent<HTMLButtonElement>): void => {
-        //create an object for Movie data and pass it through props.nominate function
-        props.nominate(props.title)
+    const removeNomination = (event: React.MouseEvent<HTMLButtonElement>): void => {
+        //create an object for Movie data and pass it through props.remove movie function
+        console.log('implementing')
     }
 
     return (
-        <Card className={classes.root}>
+        <Card >
             <CardActionArea>
                 {/* <CardMedia
                     className={classes.media}
@@ -48,16 +24,16 @@ const MovieCard = (props:Movie) => {
                 /> */}
                 <CardContent>
                     <Typography variant='h5' align='center'>
-                        {props.title}
+                        {/* {props.title} */}
                     </Typography>
                     <Typography variant='h6' align='center'>
-                        {props.year}
+                        {/* {props.year} */}
                     </Typography>
                 </CardContent>
             </CardActionArea>
             <CardActions>
-                <Button size='small' color='primary' onClick={(event) => nominateMovie(event)}>
-                   Nominate
+                <Button size='small' color='primary' onClick={(event) => removeNomination(event)}>
+                   Remove
                 </Button>
             </CardActions>
         </Card>
@@ -65,4 +41,5 @@ const MovieCard = (props:Movie) => {
 }
 
 
-export default MovieCard
+
+export default NominateCard
