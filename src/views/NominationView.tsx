5da8af71c85a7a3a60f74f650f2e1ad7e957f07c
JorPoon <jormpoon@gmail.com>
2020-09-05 23:42:24 -0400

    created a better way to generate keys and implement the foundation of banner

--- src/views/NominationView.tsx
+++ src/views/NominationView.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import NominateCard from '../components/movies/NominateCard'
 
 interface NominatedMovies {
@@ -7,10 +7,24 @@ interface NominatedMovies {
 
 const NominationView = (props:NominatedMovies) => {
 
+    const [banner, setBanner] = useState(false)
+
+    const generateKey = (pre:string) => {
+        return `${ pre }_${ new Date().getTime() }`;
+    }
+
+    useEffect(() => {
+        if(props.nominated.length >= 5) {
+            console.log(5)
+            setBanner(true)
+        }
+    }, [props.nominated])
+
     return (
         <>
-            {props.nominated.map((movie: any) => {
-                const id = Date.now()
+            {banner && <p>hi</p>}
+            {props.nominated.map((movie: any) => {  
+                const id = generateKey(movie.title)      
                 return (
                     <NominateCard 
                         key={id}
