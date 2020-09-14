eb521199cae26fd8d0d3d68850ae92cd80cc7d74
JorPoon <jormpoon@gmail.com>
2020-09-14 19:11:14 -0400

    modified public folder and changed some card codes

--- src/components/movies/MovieCard.tsx
+++ src/components/movies/MovieCard.tsx
@@ -3,24 +3,11 @@ import Card from '@material-ui/core/Card'
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 import useCardStyle from '../../styles/CardStyle'
 
-// import { makeStyles } from '@material-ui/core/styles';
-
-// const useCardStyle = makeStyles({
-//     root: {
-//       width: '300px',
-//       margin: '10px 0'
-//     },
-//     media: {
-//         paddingTop: '56.25%',
-//         height: '450px'
-//     }
-//   });
   
 
 
